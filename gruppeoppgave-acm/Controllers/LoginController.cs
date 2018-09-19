c42bd42cb10030643925cd882af8eca3d15def71
magnusihle <s315276@stud.hioa.no>
2018-09-19 16:46:53 +0200

    Fixed bug login

--- gruppeoppgave-acm/Controllers/LoginController.cs
+++ gruppeoppgave-acm/Controllers/LoginController.cs
@@ -8,7 +8,7 @@ using System.Web.Mvc;
 namespace gruppeoppgave_acm.Controllers
 {
     public class LoginController : Controller
-    { 
+    {
         [HttpGet]
         public ActionResult Login()
         {
@@ -19,21 +19,16 @@ namespace gruppeoppgave_acm.Controllers
         [HttpPost]
         public ActionResult Login(Customer loginModel)
         {
-            using(DB db = new DB())
+            using (DB db = new DB())
             {
-                if (db.Customer.Any(bruker => bruker.Username != loginModel.Username))
+                if (db.Customer.Any(bruker => bruker.Username == loginModel.Username) && db.Customer.Any(bruker => bruker.Password == loginModel.Password))
                 {
-                    ViewBag.LoginUserErr = "Login failed. Check username or password";
-                    return View("Login");
-                } if (db.Customer.Any(bruker => bruker.Password != loginModel.Password))
-                {
-                    ViewBag.LoginUserErr = "Login failed. Check username or password";
-                    return View("Login");
+                    ViewBag.LoginSuccessAlert = "Login Successful!";
+                    return View("LoggedIn");
                 }
+                ViewBag.LoginUserErr = "Login failed. Check username or password";
+                return View("Login");
             }
-            ViewBag.LoginSuccessAlert = "Login Successful!";
-            return View("LoggedIn");
-            
         }
     }
 }
