1a6aee7e0138a78500d4f320fcd39453e7587c98
Axel Brynjulf Bjørnstad <s315322@oslomet.no>
2018-09-21 09:39:56 +0200

    Code cleanup and movie index query

--- gruppeoppgave-acm/Scripts/movieloader.js
+++ gruppeoppgave-acm/Scripts/movieloader.js
@@ -1,13 +1,6 @@
-﻿
-/*
- * $("#@Html.IdFor(model => model.Postnummer)").change(function () {
-
-});
-
-*/
-
-
-
+﻿/**
+ *  Filter on  categories 
+ */
 $(".cat-checkbox").change(function () {
     if (this.checked) {
         $.ajax({
@@ -25,18 +18,25 @@ $(".cat-checkbox").change(function () {
 });
 
 
-
-$(".movie-thumbnail").click(function () {
+/**
+ * Buy movie
+ */
+$(".movie-thumbnail2").click(function () {
     $.ajax({
         type: "GET",
         url: "/Home/BuyMovie",
         data: "movieID=" + this.id
     }).done(function (response) {
             console.log("Resp: " + response);
-        
     });
 })
 
 
-
-
+/**
+ *  Extend movie view
+ */
+$(".movie-thumbnail").click(function (element) {
+    var movies = $("#movies").children();
+    var parent = element.target.offsetParent;
+    console.log(movies.index(parent))
+})
