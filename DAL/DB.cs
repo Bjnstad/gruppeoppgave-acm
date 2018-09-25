800a7c5bde0ed8dcf177f51cf3eb4057c3eb101d
Axel Brynjulf Bjørnstad <s315322@oslomet.no>
2018-09-25 22:53:14 +0200

    layering setup

---
+++ DAL/DB.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using oslomet_film.Models;
+
+namespace DAL
+{
+    public class DB : DbContext
+    {
+        public DB() : base("name=ACM")
+        {
+            Database.CreateIfNotExists();
+            //Database.SetInitializer(new DBInit());
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public virtual DbSet<Movie> Movie { get; set; }
+    }
+}
---
+++ Model/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace oslomet_film.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+        public string Thumbnail { get; set; }
+        //public virtual List<OrderLine> OrderLines { get; set; }
+        //public virtual List<Category_Relation> Category_Relation { get; set; }
+    }
+}
