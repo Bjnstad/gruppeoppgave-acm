c9e6f76b7b376516707754d31e941b4cf65da25d
Axel Bjørnstad <axebjo@gmail.com>
2018-10-07 19:25:08 +0200

    cleanup

--- oslomet-film/Scripts/main.js
+++ oslomet-film/Scripts/main.js
@@ -1,7 +1,9 @@
 ﻿/**
  *  Filter on  categories 
  */
-$(".category").click(refreshMovies(this.id));
+$(".category").click(function () {
+    refreshMovies(this.id)
+});
 
 /**
  * Add movie to cart
@@ -48,101 +50,18 @@ function refreshMovies(categorId) {
     });
 }
 
-
-
-
-
-
-
-
-
-
-
-
 /**
- * Buy movies
+ *  Open/close cart
  */
-$("buy-movies").click(function () {
-    $.ajax({
-        type: "GET",
-        url: "/Cart/CreateOrder"
-    }).done(function (response) {
-        console.log(response)
-    });
+$("#cart").click(function (e) {
+    $(".cart-popout-open").toggleClass("hide show").toggleClass("cart-popoup-open cart-popoup-close");
+    e.stopPropagation();
 });
 
-
-
-/**
- *  Open/close collapse - movie details
- */
-$(function () {
-    $(".detailsModal").click(function () {
-        $("#displayModalContent").show();
-    });
-    $(".modal-close").click(function () {
-        $("#displayModalContent").hide();
-    });
-});
-/**
- *  Open/close cart not working
- */
-$(function () {
-    $("#cart").click(function (e) {
-        $.ajax({
-            type: "GET",
-            url: "/Cart/GetTotal"
-        })
-        $(".cart-popout-open").toggleClass("hide show").toggleClass("cart-popoup-open cart-popoup-close");
-        e.stopPropagation();
-    });
-
-    $(".cart-popoup-close").click(function (e) {
-        e.stopPropagation();
-    });
-
-    $(document).click(function () {
-        $(".cart-popout-close").toggleClass("show hide").toggleClass("cart-popoup-close cart-popoup-open");
-    });
-    
+$(".cart-popoup-close").click(function (e) {
+    e.stopPropagation();
 });
 
-/**
- * Add movie to cart
- */
-$(".movie-thumbnail").click(function () {
-    $.ajax({
-        type: "GET",
-        url: "/Cart/AddItem",
-        data: "movieID=" + this.id
-    }).done(function (response) {
-        $("#cart").html(response)
-    });
-}); 
-/**
- *  Extend movie view
- */
-$(".movie-thumbnail2").click(function (element) {
-    $.ajax({
-        type: "GET",
-        url: "/Movie/GetMovie",
-        data: "movieID=" + this.id
-    }).done(function (response) {
-        // TODO: Check failed respons
-
-
-        var movies = $("#movies").children();
-        var parent = element.target.offsetParent;
-        var index = movies.index(parent);
-
-        console.log(movies[index]);
-        var e = document.getElementById("movies");
-        
-
-        
-        var div = document.createElement('div');
-        $(div).html(response);
-
-        $("#movies").append(response.trim());
-    });
-})
+$(document).click(function () {
+    $(".cart-popout-close").toggleClass("show hide").toggleClass("cart-popoup-close cart-popoup-open");
+});
